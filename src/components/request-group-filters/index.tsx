89d1e27de4004f85e54476f66808a680a46941d5
Noam Malka <noamalka2903@gmail.com>
2025-08-23 13:53:14 +0300

    Added basic box for filters

--- src/components/request-group-filters/index.tsx
+++ src/components/request-group-filters/index.tsx
@@ -1,5 +1,30 @@
+import {
+  Box,
+  Button,
+  Typography,
+} from "@mui/material";
+
 interface RequestGroupFiltersProps { }
 
 export const RequestGroupFilters: React.FC<RequestGroupFiltersProps> = ({ }) => {
-  return <></>;
+  return (
+    <Box
+      component="section"
+      sx={{
+        maxWidth: 1280,
+        mx: "auto",
+        position: "relative",
+        color: "white",
+        borderColor: "#e5e7eb",
+        overflow: "hidden",
+        boxShadow: 3,
+        borderRadius: 1,
+        background:
+          "rgba(255, 255, 255, 0.96)",
+        py: { xs: 6, md: 8 },
+        px: { xs: 2, md: 4 },
+      }}
+    >
+    </Box>
+  );
 };
