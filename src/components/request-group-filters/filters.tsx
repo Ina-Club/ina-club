080a756c629c7bf99a1f8e8e94c1620212a4f983
Noam Malka <noamalka2903@gmail.com>
2025-08-26 23:26:34 +0300

    Added slider for price range

--- src/components/request-group-filters/filters.tsx
+++ src/components/request-group-filters/filters.tsx
@@ -2,18 +2,28 @@ import {
   Box,
   TextField,
   MenuItem,
-  InputAdornment,
+  Slider,
   Typography,
 } from "@mui/material";
 import { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 interface FiltersProps { }
 
 export const Filters: React.FC<FiltersProps> = ({ }) => {
+  const minPrice = 20;
+  const maxPrice = 80;
+  const rtlTheme = createTheme({ direction: "rtl" });
   const [category, setCategory] = useState('all');
-  const [price, setPrice] = useState('');
   const [location, setLocation] = useState('all');
   const [popularity, setPopularity] = useState('all');
+  const [priceRange, setPriceRange] = useState<{ min: number; max: number }>({ min: minPrice, max: maxPrice });
+
+  const handlePriceRangeChange = (_: Event, newValue: number | number[]) => {
+    if (Array.isArray(newValue)) {
+      setPriceRange({ min: newValue[0], max: newValue[1] });
+    }
+  };
 
   return (
     <>
@@ -57,91 +67,6 @@ export const Filters: React.FC<FiltersProps> = ({ }) => {
             <MenuItem value="clothing">ביגוד</MenuItem>
             <MenuItem value="food">מזון</MenuItem>
           </TextField>
-        </Box >
-        {/* מחיר */}
-        <Box>
-          <Typography>החל מ</Typography>
-          <TextField
-            variant="outlined"
-            fullWidth
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                borderRadius: "10px",
-                height: "45px",
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "#BED6E9",
-                },
-                "&:hover .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "#BED6E9",
-                },
-                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "#BED6E9",
-                  borderWidth: "1px",
-                },
-              },
-              "& .MuiInputBase-input": {
-                fontSize: "18px",
-                padding: "0 10px",
-                textAlign: "right",
-              },
-              "& .MuiSelect-icon": {
-                left: "7px",
-                right: "auto",
-              },
-            }}
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">₪</InputAdornment>
-                ),
-              },
-            }}
-          ></TextField>
-        </Box >
-
-        {/* מחיר */}
-        < Box>
-          <Typography>עד</Typography>
-          <TextField
-            variant="outlined"
-            fullWidth
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                borderRadius: "10px",
-                height: "45px",
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "#BED6E9",
-                },
-                "&:hover .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "#BED6E9",
-                },
-                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "#BED6E9",
-                  borderWidth: "1px",
-                },
-              },
-              "& .MuiInputBase-input": {
-                fontSize: "18px",
-                padding: "0 10px",
-                textAlign: "right",
-              },
-              "& .MuiSelect-icon": {
-                left: "7px",
-                right: "auto",
-              },
-            }}
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">₪</InputAdornment>
-                ),
-              },
-            }}
-          ></TextField>
         </Box >
 
         {/* מיקום */}
@@ -224,6 +149,26 @@ export const Filters: React.FC<FiltersProps> = ({ }) => {
             <MenuItem value="new">חדש</MenuItem>
           </TextField>
         </Box >
+
+        {/* טווח מחיר */}
+        <ThemeProvider theme={rtlTheme}>
+          <Box sx={{ width: 300, mx: "auto" }}>
+            <Slider
+              value={[priceRange.min, priceRange.max]}
+              onChange={handlePriceRangeChange}
+              valueLabelDisplay="auto"
+              disableSwap
+              min={minPrice}
+              max={maxPrice}
+              sx={{
+                boxSizing: 'content-box',
+                '& .MuiSlider-thumb': { width: 20, height: 20 }, // document the size you matched
+              }}
+            />
+            <Typography>{priceRange.min}</Typography>
+            <Typography>{priceRange.max}</Typography>
+          </Box>
+        </ThemeProvider>
       </Box>
     </>
   );
