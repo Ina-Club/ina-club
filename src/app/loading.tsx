429ccd70bcc935aaa0d8e6c91abaf66f75e416f8
Noam Malka <noamalka2903@gmail.com>
2025-08-29 18:43:04 +0300

    Removed unnecessary imports

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,3 @@
-import { Hero } from "@/components/hero";
-import RequestGroupSectionSkeleton from "@/components/skeleton/request-group-section-skeleton";
-import RequestGroupSectionWrapper from "@/components/wrapper/request-group-section-wrapper";
-import { Container } from "@mui/material";
-import { Suspense } from "react";
-
 export default function Loading() {
   return (
     <div>
