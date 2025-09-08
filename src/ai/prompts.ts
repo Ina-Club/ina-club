10509b7477c5af20ba068a3dcfbca3ee6b561ffb
Noam Malka <noamalka2903@gmail.com>
2025-09-09 01:26:49 +0300

    Added improvements to the prompt and display

--- src/ai/prompts.ts
+++ src/ai/prompts.ts
@@ -1,9 +1,13 @@
 export const PRICE_ANALYZER_PROMPT: string =
     `
     Generate a JSON object for a {productName}.
-    If a specific model is provided, search for its price online and present an average price range.
+    You are a market researcher expert that is familiar with all kinds of products.
+    I want you to conduct a research about {productName}.
+    Search about each of the properties provided in the following list: {propertyList}.
+    When you finish I want you to deliver me the output as followed:
+    Return a JSON object that represents an item where each detail is presented as a field in the object.
+    Couple of things to keep in mide: if a specific model is provided, search for its price online and present an average price range.
     If no specific model is provided, select the highest-rated product in that category, perform a full price analysis for it, and present the information in JSON format.
-    The final output should include the following details: {propertyList}.
-    Return an object for an item with the average price across different websites in Israel.
-    The price should be in ILS. if in the items there is no a spesific model return the higest rated item for the same family.
+    All string-type details should be written in Hebrew and related to Israel (For example, 'average price' property is based on different websites in Israel and in ILS).
+    For prices, don't include the unit or the sign of ILS, just make sure the number is in the right ILS amount. Also, the should be rounded to the nearest integer.
     `
--- src/components/loading-circle/index.tsx
+++ src/components/loading-circle/index.tsx
@@ -11,7 +11,7 @@ export const LoadingCircle: React.FC<LoadingCircleProps> = ({ loadingText }) => 
     return (
         <Box sx={{ display: 'flex', flexDirection: "column", alignItems: "center", pb: { xs: 2, md: 4 } }}>
             {loadingText &&
-                <Typography>{loadingText}</Typography>
+                <Typography sx={{ pb: { xs: 1, md: 2 } }}>{loadingText}</Typography>
             }
             <CircularProgress />
         </Box>
