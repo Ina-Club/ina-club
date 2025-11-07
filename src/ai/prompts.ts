9f7401a547cd96005910bdf3fd4eb5e683dcf2a4
Noam Malka <noamalka2903@gmail.com>
2025-11-07 17:39:37 +0200

    Created a relevant prompt

--- src/ai/prompts.ts
+++ src/ai/prompts.ts
@@ -1,15 +1,13 @@
 export const SMART_SEARCH_PROMPT: string =
     `
-    Generate a JSON object for a {productName}.
-    You are a market researcher expert that is familiar with all kinds of products.
-    I want you to conduct a research about {productName}.
-    Search about each of the properties provided in the following list: {propertyList}.
-    When you finish I want you to deliver me the output as followed:
-    Return a JSON object that represents an item where each detail is presented as a field in the object.
-    Couple of things to keep in mide: if a specific model is provided, search for its price online and present an average price range.
-    If no specific model is provided, select the highest-rated product in that category, perform a full price analysis for it, and present the information in JSON format.
-    All string-type details should be written in Hebrew and related to Israel (For example, 'average price' property is based on different websites in Israel and in ILS).
-    For prices, don't include the unit or the sign of ILS, just make sure the number is in the right ILS amount.
-    Also, prices should be rounded to the nearest integer.
-    If any of the specified fields are invalid/not found/not available for some reason and you think that you might provide false information, return an empty Dict.
+    You are a software engineering expert which is also great at associations.
+    I want you to run over 2 lists:
+    1. requestGroups list: {requestGroups}
+    2. activeGroups list: {activeGroups}
+    Since I am sending you an HTTP request, the return type you should hand me is provided to you in the schema property of the request's body. 
+    I want you to run over all the elements in both lists and filter out all elements except the one that any of their properties have a reasonable connection to {searchText}.
+    From the remaining group items, insert ONLY their id's to the relevant array that should be returned and is defined in your responseSchema.
+    For example, if you found 1 requestGroup that match the provided text, and assuming we can call the object rg, the requestGroups key of the returned object should have a value of [rg.id], and activeGroups should have value of [].
+    If you can't find any connection, return for both properties empty arrays.
+    For any other problem you face that prevents you from returning the exact response structure that I asked for, return an empty object.
     `
