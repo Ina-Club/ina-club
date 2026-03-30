8bd0247345a34721f93d319abc8be5ac62c4fdc8
Noam Malka <noamalka2903@gmail.com>
2026-03-30 13:06:09 +0300

    Added opacity change for activated groups

--- src/components/card/active-group-card/index.tsx
+++ src/components/card/active-group-card/index.tsx
@@ -30,14 +30,16 @@ const ActiveGroupCard: React.FC<ActiveGroupCardProps> = ({ activeGroup }) => {
         flex: 1,
         overflowWrap: "anywhere",
         overflow: "hidden",
-        transition: "transform 0.25s, box-shadow 0.25s",
+        transition: "transform 0.25s, box-shadow 0.25s, opacity 0.25s",
         display: "flex",
         flexDirection: "column",
         bgcolor: "background.paper",
+        opacity: activeGroup.status === "ACTIVATED" ? 0.65 : 1,
         "&:hover": {
           transform: "translateY(-6px)",
           boxShadow: 8,
           cursor: "pointer",
+          opacity: activeGroup.status === "ACTIVATED" ? 0.85 : 1,
         },
       }}
       onClick={goToActiveGroup}
